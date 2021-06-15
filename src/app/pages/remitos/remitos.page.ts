f5fdf19402913d3eabeff0039f3b24380a4909d2
joniim23 <jonathan.mansi@gmail.com>
2021-06-15 19:42:47 -0300

    Se devuelve la lista de remitos por idUsuario

--- src/app/pages/remitos/remitos.page.ts
+++ src/app/pages/remitos/remitos.page.ts
@@ -2,30 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { RemitoService } from '../../services/remito.service';
 import { Router } from '@angular/router';
 import { Remito } from 'src/app/models/remito.models';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Usuario } from 'src/app/models/usuario.models';
 
 @Component({
   selector: 'app-remitos',
   templateUrl: './remitos.page.html',
   styleUrls: ['./remitos.page.scss'],
 })
-export class RemitosPage implements OnInit {
-
+export class RemitosPage {
+  
+  remito: Remito;
   remitos: Remito[];
+  user: Usuario;
 
-  constructor(private remitoService: RemitoService, private router: Router) { }
+  constructor(
+    private remitoService: RemitoService, 
+    private router: Router,
+    private authService: AuthenticationService
+  ) { }
 
-  /* Este método se ejecuta cuando se genera el componente por primera vez */
-  async ngOnInit() {
-    //let idCliente = await this..getCliente();
-    this.remitoService.getAll(1, 'Pendiente').then(
-      //this.remitoService.getAll(idCliente, 'Pendiente').then(
-      (data: Remito[]) => this.remitos = data
-    );
-  }
 
   /* Este método se ejecuta cada vez que se entra al componente  */
   async ionViewWillEnter() {
-    this.remitoService.getAll(1, 'Pendiente').then(
+    const user = await this.authService.getUser();
+    this.user = JSON.parse(user);
+
+    this.remitoService.getAll(this.user.idUsuario).then(
       //this.remitoService.getAll(idCliente, 'Pendiente').then(
       (data: Remito[]) => this.remitos = data
     );
--- src/app/services/remito.service.ts
+++ src/app/services/remito.service.ts
@@ -1,17 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Remito } from '../models/remito.models';
-import { Storage } from '@ionic/storage-angular';
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RemitoService {
+  idUsuario: number;
+  url = environment.apiUrl + '/remito';
+  constructor(
+    private http: HttpClient,
+    private authService: AuthenticationService
+  ) { 
+    this.authService.loadsData();
+    let user = this.authService.getUser();
+    this.idUsuario = JSON.parse(user).idUsuario;
+  }
 
-  url = 'http://localhost:8080/remito';
-  constructor(private http: HttpClient, private storage: Storage) { }
+  async getAll(idUsuario: number): Promise<Remito[]> {
+    console.log("Buscaré los remitos del usuario " + this.idUsuario)
+    return await this.http.get<Remito[]>(this.url+ '/all/' + this.idUsuario).toPromise()
+  }
 
-  async getAll(idCliente: number, estado: string): Promise<Remito[]> {
+  async getAllRemitosPorCliente(idCliente: number, estado: string): Promise<Remito[]> {
+    console.log("Buscaré los remitos del cliente " + idCliente);
     return this.http.get<Remito[]>(`${this.url}/all/?idCliente=${idCliente}&estado=${estado}`).toPromise();
   }
 
